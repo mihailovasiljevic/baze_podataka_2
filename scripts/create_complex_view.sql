059191e13e2e67cdcac122dcb2db86222bbf77a0
Mihailo <mihailo.vasiljevic93@gmail.com>
2016-01-26 03:18:47 +0100

    Changed data

--- scripts/create_complex_view.sql
+++ scripts/create_complex_view.sql
@@ -16,7 +16,7 @@ WHERE UOP.POSR_DAT_IST >= to_date(SYSDATE, 'DD-MON-YY');
 
 -- POGLED: Prika?i sve adrese
 CREATE OR REPLACE VIEW
-adresa_nepokretnosti(Ulica, Broj, Grad, Dr?ava) AS
+adresa_nepokretnosti(Ulica, Broj, Grad, Drzava) AS
 SELECT U.ULICA_NAZ, A.ADRS_ULBR, D.DRZ_NAZ, G.LOK_NAZ 
 FROM DRZAVA D, GRAD G, ULICA U, ADRESA A
 WHERE A.ULICA_ID = U.ULICA_ID (+) AND A.LOK_ID = G.LOK_ID(+) AND A.DRZ_ID = D.DRZ_ID (+)
@@ -25,12 +25,12 @@ GROUP BY  U.ULICA_NAZ, A.ADRS_ULBR, D.DRZ_NAZ, G.LOK_NAZ;
 
 --POGLED 3: Prika?i dodatne informacije o nepokretnosti
 CREATE OR REPLACE VIEW
-citava_nepokretnost(Oznaka,Povr?ina, Useljiva, "U izgradnji", "Broj soba", "Broj spratova", Dupleks, 
+citava_nepokretnost(Oznaka,Povrsina, Useljiva, "U izgradnji", "Broj soba", "Broj spratova", Dupleks, 
           "Ima lift", "Stara gradnja", Legalna, Tajna, Prodata, Opis, "Tip nepokretnosti", "Oznaka prodavca",
-          "Ime prodavca", "Prezime prodavca", "Broj li?ne karte prodavca", "Telefon prodavca", "Email prodavca", Ulica,Broj,Grad, Drzava, "Ukupni troskovi", "Ukupno slika"
+          "Ime prodavca", "Prezime prodavca", "Broj licne karte prodavca", "Telefon prodavca", "Email prodavca", Ulica,Broj,Grad, Drzava, "Ukupni troskovi", "Ukupno slika"
 ) AS
 SELECT DISTINCT N.NEK_ID, N.NEK_POV, N.NEK_USELJ, N.NEK_UIZGR, N.NEK_SOBE, N.NEK_SPRAT, N.NEK_DUPL, N.NEK_LIFT, N.NEK_STARA, N.NEK_LEG,
-       N.NEK_TAJNA, N.NEK_PROD, N.NEK_OPIS, T.NEK_TIP_NAZ, P.PROD_ID, P.PROD_IME, P.PROD_PREZ, P.PROD_BRLK, P.PROD_TEL, P.PROD_EMAIL, U.ULICA_NAZ, A.ADRS_ULBR, G.LOK_NAZ, D.DRZ_NAZ, sum(UOP.POSR_TROSK), nvl(count(SL.SLIKA_ID), '0')
+       N.NEK_TAJNA, N.NEK_PROD, N.NEK_OPIS, T.NEK_TIP_NAZ, P.PROD_ID, P.PROD_IME, P.PROD_PREZ, P.PROD_BRLK, P.PROD_TEL, P.PROD_EMAIL, U.ULICA_NAZ, A.ADRS_ULBR, G.LOK_NAZ, D.DRZ_NAZ, UOP.POSR_TROSK, nvl(count(SL.SLIKA_ID), '0')
 FROM NEPOKRETNOST N, TIP_NEPOKRETNOSTI T, PRODAVAC P, ULICA U, GRAD G, DRZAVA D, ADRESA A, UGOVOR_O_POSREDOVANJU UOP, SLIKA SL
 WHERE
       P.PROD_ID = N.PROD_ID  AND
@@ -47,5 +47,5 @@ group by N.NEK_ID, N.NEK_POV, N.NEK_USELJ, N.NEK_UIZGR, N.NEK_SOBE,
 N.NEK_SPRAT, N.NEK_DUPL, N.NEK_LIFT, N.NEK_STARA, N.NEK_LEG, 
 N.NEK_TAJNA, N.NEK_PROD, N.NEK_OPIS, T.NEK_TIP_NAZ, P.PROD_ID, 
 P.PROD_IME, P.PROD_PREZ, P.PROD_BRLK, P.PROD_TEL, P.PROD_EMAIL, 
-U.ULICA_NAZ, A.ADRS_ULBR, G.LOK_NAZ, D.DRZ_NAZ;
+U.ULICA_NAZ, A.ADRS_ULBR, G.LOK_NAZ, D.DRZ_NAZ, UOP.POSR_TROSK;
   
